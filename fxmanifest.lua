5fe6731f0251f697aebaec92ff30d24cf2dda14e
SerumXD <elledahl2002@hotmail.se>
2022-01-25 18:55:53 +0100

    2022-01-25
    Sänkt InitialDriveForce på alla bilar

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,10 @@ version '1.0.0'
 
 files {
 	'handling.meta',
+	'carcols.meta',
+	-- 'vehicles.meta',
 }
 
 data_file 'HANDLING_FILE'				'handling.meta'
+data_file 'CARCOLS_FILE'				'carcols.meta'
+-- data_file 'VEHICLE_METADATA_FILE'	'vehicles.meta'
