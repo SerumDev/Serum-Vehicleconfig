703a5b025588048db6e06556de4da25650c26b2e
SerumXD <elledahl2002@hotmail.se>
2022-01-30 17:52:27 +0100

    2022-01-30

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,12 +7,12 @@ version '1.0.0'
 
 files {
 	'handling.meta',
-	'carcols.meta',
+	-- 'carcols.meta',
 	-- 'vehicles.meta',
 	-- 'carvariations.meta'
 }
 
 data_file 'HANDLING_FILE'				'handling.meta'
-data_file 'CARCOLS_FILE'				'carcols.meta'
+-- data_file 'CARCOLS_FILE'				'carcols.meta'
 -- data_file 'VEHICLE_METADATA_FILE'	'vehicles.meta'
 -- data_file 'VEHICLE_VARIATION_FILE'	'carvariations.meta'
