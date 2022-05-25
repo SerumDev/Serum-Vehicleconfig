ec98b5f2ed311f6c2ab820521910973fbb8ac8be
SerumXD <elledahl2002@hotmail.se>
2022-05-25 20:32:55 +0200

    Champion Changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,16 +3,16 @@ game 'gta5'
 
 author 'SerumXD'
 description 'Car FxManifest'
-version '1.0.0'
+version '2.0.0'
 
 files {
 	'handling.meta',
-	-- 'carcols.meta',
-	-- 'vehicles.meta',
-	-- 'carvariations.meta'
+	'carcols.meta',
+	'carvariations.meta',
+	'vehicles.meta',
 }
 
-data_file 'HANDLING_FILE'					'handling.meta'
--- data_file 'CARCOLS_FILE'				'carcols.meta'
--- data_file 'VEHICLE_METADATA_FILE'	'vehicles.meta'
--- data_file 'VEHICLE_VARIATION_FILE'	'carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
