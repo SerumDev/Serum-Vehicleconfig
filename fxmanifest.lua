f882329d2da7f7de569845568a49bbb65e031262
SerumXD <elledahl2002@hotmail.se>
2022-11-19 00:01:09 +0100

    Update
    Horonger

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,13 @@ game 'gta5'
 
 author 'SerumXD'
 description 'Car FxManifest'
-version '2.0.0'
+version '1.0.0'
 
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
---data_file 'WEAPONINFO_FILE' 'additional/weapon_dish.meta'
 
 files {
   'vehiclelayouts.meta',
@@ -18,7 +17,6 @@ files {
   'vehicles.meta',
   'carcols.meta',
   'carvariations.meta',
---  'additional/weapon_dish.meta',
 }
 
 client_script 'vehicle_names.lua'
--- vehicle_names.lua
+++ vehicle_names.lua
@@ -3,5 +3,9 @@ function AddTextEntry(key, value)
 end
 
 Citizen.CreateThread(function()
-	AddTextEntry("NULL_SPOILER", "Aero package")
+	AddTextEntry("NULL_SPOILER", "Aero Package")
+	AddTextEntry("BILEMERU_LIV_1", "Bilfirma Logo")
+	AddTextEntry("CARAMEKO_LIV1", "Mekonomen Gul")
+	AddTextEntry("CARAMEKO_LIV2", "Mekonomen Svart")
+	AddTextEntry("NEONCOP_LIV1", "Polis Klistermärken")
 end)
