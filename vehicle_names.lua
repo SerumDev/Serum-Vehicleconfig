f257996b754f7a24b4095a77fb7c932b0aacbda6
SerumXD <elledahl2002@hotmail.se>
2023-08-28 19:11:58 +0200

    Well

--- vehicle_names.lua
+++ vehicle_names.lua
@@ -4,7 +4,4 @@ end
 
 Citizen.CreateThread(function()
 	AddTextEntry("NULL_SPOILER", "Aero Package")
-	AddTextEntry("BILEMERU_LIV_1", "Bilfirma Logo")
-	AddTextEntry("CARAMEKO_LIV1", "Mekonomen Gul")
-	AddTextEntry("CARAMEKO_LIV2", "Mekonomen Svart")
 end)
